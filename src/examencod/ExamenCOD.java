6245f8fd877ed332480d97435a0675fb0d48f655
ddizoya <ddizoya@eniac30.danielcastelao.org>
2014-12-04 09:11:50 +0100

    Refactorización: cambio de variables y métodos.

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -6,30 +6,30 @@ public class ExamenCOD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int iN = 11;
-        if (metodoMio(iN)) {
+        int numeroPrimoBase = 11;
+        if (comprobarPrimo(numeroPrimoBase)) {
             System.out.println("Es numero primo");
         } else {
             System.out.println("NO es numero primo");
         }
 
-        for (int x = 2; x < 1000; x++) {
-            if (metodoMio(x)) {
-                System.out.print(x + " ");
+        for (int i = 2; i < 1000; i++) { //He cambiado x por i porque es más 'típico' ver un bucle for con i.
+            if (comprobarPrimo(i)) {
+                System.out.print(i + " ");
             }
         }
     }
 
-    public static boolean metodoMio(int variable) {
-        int j = 2;
-        boolean aux = true;
-        while ((aux) && (j != variable)) {
-            if (variable % j == 0) {
-                aux = false;
+    public static boolean comprobarPrimo(int numeroIntroducido) {
+        int numeroPar = 2;
+        boolean esPrimo = true;
+        while ((esPrimo) && (numeroPar != numeroIntroducido)) {
+            if (numeroIntroducido % numeroPar == 0) {
+                esPrimo = false;
             }
-            j++;
+            numeroPar++;
         }
-        return aux;
+        return esPrimo;
     }
 
 }
